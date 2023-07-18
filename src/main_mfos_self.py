da211a36b24f63cde959da13787751e7ef8ddb80
L Islaam <65960611+LIslaam@users.noreply.github.com>
2023-07-18 18:00:57 +0100

    Update exp_name file path in main_mfos_self.py

--- src/main_mfos_self.py
+++ src/main_mfos_self.py
@@ -1,17 +1,18 @@
 import torch
 from ppo import PPO, Memory
-from environments import MetaGames, SymmetricMetaGames
+from environments import MetaGames
 import os
 import argparse
 import json
-import numpy as np
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--game", type=str, required=True)
+parser.add_argument("--opponent", type=str, required=True)
 parser.add_argument("--entropy", type=float, default=0.01)
 parser.add_argument("--exp-name", type=str, default="")
 parser.add_argument("--checkpoint", type=str, default="")
+parser.add_argument("--mamaml-id", type=int, default=0)
 args = parser.parse_args()
 
 
@@ -22,156 +23,91 @@ if __name__ == "__main__":
     eps_clip = 0.2  # clip parameter for PPO
     gamma = 0.99  # discount factor
 
-    lr = 0.0002  # parameters for Adam optimizer
+    lr = 0.002  # parameters for Adam optimizer
     betas = (0.9, 0.999)
 
-    max_episodes = 1024
+    max_episodes = 2048
     batch_size = 4096
     random_seed = None
     num_steps = 100
 
-    save_freq = 1000
-
-    lamb = 1.0
-    lamb_anneal = 0.0015
+    save_freq = 250
     name = args.exp_name
 
-    print(f"RUNNING NAME: {name}")
-    if not os.path.isdir(name):
-        os.mkdir(name)
-        with open(os.path.join(name, "commandline_args.txt"), "w") as f:
+    print(f"RUNNING NAME: {'runs/' + name}")
+    if not os.path.isdir('runs/' + name):
+        os.mkdir('runs/' + name)
+        with open(os.path.join('runs/' + name, "commandline_args.txt"), "w") as f:
             json.dump(args.__dict__, f, indent=2)
 
     #############################################
 
     # creating environment
-    env = SymmetricMetaGames(batch_size, game=args.game)
+    env = MetaGames(batch_size, opponent=args.opponent, game=args.game, mmapg_id=args.mamaml_id)
 
     action_dim = env.d
     state_dim = env.d * 2
 
-    memory_0 = Memory()
-    memory_1 = Memory()
+    memory = Memory()
+    ppo = PPO(state_dim, action_dim, lr, betas, gamma, K_epochs, eps_clip, args.entropy)
 
-    ppo_0 = PPO(state_dim, action_dim, lr, betas, gamma, K_epochs, eps_clip, entropy=args.entropy)
-    ppo_1 = PPO(state_dim, action_dim, lr, betas, gamma, K_epochs, eps_clip, entropy=args.entropy)
-
-    nl_env = MetaGames(batch_size, game=args.game)
+    if args.checkpoint:
+        ppo.load(args.checkpoint)
 
     print(lr, betas)
+
+    # logging variables
+    running_reward = 0
+    avg_length = 0
+    time_step = 0
+
     # training loop
     rew_means = []
 
     for i_episode in range(1, max_episodes + 1):
-        print("=" * 100)
-        print(i_episode)
-        print(lamb, flush=True)
-        if lamb > 0:
-            lamb -= lamb_anneal
-        if np.random.random() > lamb:
-            print("v opponent")
-            state_0, state_1 = env.reset()
+        state = env.reset()
 
-            running_reward_0 = torch.zeros(batch_size).cuda()
-            running_reward_1 = torch.zeros(batch_size).cuda()
+        running_reward = torch.zeros(batch_size).cuda()
+        running_opp_reward = torch.zeros(batch_size).cuda()
 
-            for t in range(num_steps):
+        last_reward = 0
 
-                # Running policy_old:
-                action_0 = ppo_0.policy_old.act(state_0, memory_0)
-                action_1 = ppo_1.policy_old.act(state_1, memory_1)
-                states, rewards, M = env.step(action_0, action_1)
-                state_0, state_1 = states
-                reward_0, reward_1 = rewards
+        for t in range(num_steps):
 
-                memory_0.rewards.append(reward_0)
-                memory_1.rewards.append(reward_1)
+            # Running policy_old:
+            action = ppo.policy_old.act(state, memory)
+            state, reward, info, M = env.step(action)
 
-                running_reward_0 += reward_0.squeeze(-1)
-                running_reward_1 += reward_1.squeeze(-1)
+            memory.rewards.append(reward)
+            running_reward += reward.squeeze(-1)
+            running_opp_reward += info.squeeze(-1)
+            last_reward = reward.squeeze(-1)
 
-            ppo_0.update(memory_0)
-            ppo_1.update(memory_1)
+        ppo.update(memory)
+        memory.clear_memory()
 
-            memory_0.clear_memory()
-            memory_1.clear_memory()
+        print("=" * 100, flush=True)
 
-            l0 = -running_reward_0.mean() / num_steps
-            l1 = -running_reward_1.mean() / num_steps
-            print(f"loss 0: {l0}")
-            print(f"loss 1: {l1}")
-            print(f"sum: {l0 + l1}")
+        print(f"episode: {i_episode}", flush=True)
 
-            rew_means.append(
-                {
-                    "ep": i_episode,
-                    "other": True,
-                    "rew 0": -l0.item(),
-                    "rew 1": -l1.item(),
-                }
-            )
+        print(f"loss: {-running_reward.mean() / num_steps}", flush=True)
 
-        else:
-            print("v nl")
-            state = nl_env.reset()
+        rew_means.append(
+            {
+                "rew": (running_reward.mean() / num_steps).item(),
+                "opp_rew": (running_opp_reward.mean() / num_steps).item(),
+            }
+        )
 
-            running_reward_0 = torch.zeros(batch_size).cuda()
-            running_opp_reward = torch.zeros(batch_size).cuda()
-
-            for t in range(num_steps):
-                # Running policy_old:
-                action = ppo_0.policy_old.act(state, memory_0)
-                state, reward, info, M = nl_env.step(action)
-
-                memory_0.rewards.append(reward)
-                running_reward_0 += reward.squeeze(-1)
-                running_opp_reward += info.squeeze(-1)
-
-            ppo_0.update(memory_0)
-            memory_0.clear_memory()
-
-            print(f"loss 0: {-running_reward_0.mean() / num_steps}")
-            print(f"opponent loss 0: {-running_opp_reward.mean() / num_steps}")
-
-            # SECOND AGENT UPDATE
-            state = nl_env.reset()
-
-            running_reward_1 = torch.zeros(batch_size).cuda()
-            running_opp_reward = torch.zeros(batch_size).cuda()
-
-            for t in range(num_steps):
-                # Running policy_old:
-                action = ppo_1.policy_old.act(state, memory_1)
-                state, reward, info, M = nl_env.step(action)
-
-                memory_1.rewards.append(reward)
-                running_reward_1 += reward.squeeze(-1)
-                running_opp_reward += info.squeeze(-1)
-
-            ppo_1.update(memory_1)
-            memory_1.clear_memory()
-
-            print(f"loss 1: {-running_reward_1.mean() / num_steps}")
-            print(f"opponent loss 1: {-running_opp_reward.mean() / num_steps}")
-
-            rew_means.append(
-                {
-                    "ep": i_episode,
-                    "other": False,
-                    "rew 0": (running_reward_0.mean() / num_steps).item(),
-                    "rew 1": (running_reward_1.mean() / num_steps).item(),
-                }
-            )
+        print(f"opponent loss: {-running_opp_reward.mean() / num_steps}", flush=True)
 
         if i_episode % save_freq == 0:
-            ppo_0.save(os.path.join(name, f"{i_episode}_0.pth"))
-            ppo_1.save(os.path.join(name, f"{i_episode}_1.pth"))
-            with open(os.path.join(name, f"out_{i_episode}.json"), "w") as f:
+            ppo.save(os.path.join('runs/' + name, f"{i_episode}.pth"))
+            with open(os.path.join('runs/' + name, f"out_{i_episode}.json"), "w") as f:
                 json.dump(rew_means, f)
             print(f"SAVING! {i_episode}")
 
-    ppo_0.save(os.path.join(name, f"{i_episode}_0.pth"))
-    ppo_1.save(os.path.join(name, f"{i_episode}_1.pth"))
-    with open(os.path.join(name, f"out_{i_episode}.json"), "w") as f:
+    ppo.save(os.path.join('runs/' + name, f"{i_episode}.pth"))
+    with open(os.path.join('runs/' + name, f"out_{i_episode}.json"), "w") as f:
         json.dump(rew_means, f)
     print(f"SAVING! {i_episode}")
