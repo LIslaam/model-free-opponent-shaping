d5c71cc2e8733b9a2633d78e4fb7d8ca8363ffdc
L Islaam <65960611+LIslaam@users.noreply.github.com>
2023-07-18 17:56:28 +0100

    Testing trained M-FOS agent on IPD or IMP

---
+++ src/test_mfos_ppo.py
@@ -0,0 +1,107 @@
+# Test a pretrained PPO agent
+import torch
+from ppo import PPO, Memory
+from environments import MetaGames
+import os
+import argparse
+import json
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--game", type=str, required=True)
+parser.add_argument("--opponent", type=str, required=True)
+parser.add_argument("--entropy", type=float, default=0.01)
+parser.add_argument("--exp-name", type=str, default="")
+parser.add_argument("--checkpoint", type=str, default="")
+parser.add_argument("--mamaml-id", type=int, default=0)
+args = parser.parse_args()
+
+if __name__ == "__main__":
+            ##############################
+    K_epochs = 4  # update policy for K epochs
+
+    eps_clip = 0.2  # clip parameter for PPO
+    gamma = 0.99  # discount factor
+
+    lr = 0.002  # parameters for Adam optimizer
+    betas = (0.9, 0.999)
+
+    max_episodes = 256
+    batch_size = 4096
+    random_seed = None
+    num_steps = 100
+    num_tests = 10 # How many times trained policy is tested on game
+
+    save_freq = 250
+    name = args.exp_name
+
+    print(f"RUNNING NAME: {'runs/' + name + '/test_' + args.game}")
+    if not os.path.isdir('runs/' + name + '/test_' + args.game):
+        os.mkdir('runs/' + name + '/test_' + args.game)
+        with open(os.path.join('runs/' + name + '/test_' + args.game, 
+                               "commandline_args.txt"), "w") as f:
+            json.dump(args.__dict__, f, indent=2)
+
+    #################################################
+
+    env = MetaGames(batch_size, opponent=args.opponent, game=args.game, mmapg_id=args.mamaml_id)
+    memory = Memory()
+    
+    action_dim = env.d
+    state_dim = env.d * 2
+
+    ppo = PPO(state_dim, action_dim, lr, betas, gamma, K_epochs, eps_clip, args.entropy)
+
+    directory = "runs/" + name + '/'
+    checkpoint_path = directory + "{}.pth".format(32768)# max_episodes)
+    print("loading network from : " + checkpoint_path)
+
+    ppo.load(checkpoint_path)
+    rew_means = []
+
+    for i_episode in range(1, max_episodes + 1):
+        state = env.reset()
+
+        running_reward = torch.zeros(batch_size).cuda()
+        running_opp_reward = torch.zeros(batch_size).cuda()
+
+        last_reward = 0
+
+        for t in range(num_steps):
+
+            # Running policy_old:
+            action = ppo.policy_old.act(state, memory)
+            state, reward, info, M = env.step(action)
+
+            memory.rewards.append(reward)
+            running_reward += reward.squeeze(-1)
+            running_opp_reward += info.squeeze(-1)
+            last_reward = reward.squeeze(-1)
+
+        memory.clear_memory()
+
+        print("=" * 100, flush=True)
+
+        print(f"episode: {i_episode}", flush=True)
+
+        print(f"loss: {-running_reward.mean() / num_steps}", flush=True)
+
+        rew_means.append(
+            {
+                "rew": (running_reward.mean() / num_steps).item(),
+                "opp_rew": (running_opp_reward.mean() / num_steps).item(),
+            }
+        )
+
+        print(f"opponent loss: {-running_opp_reward.mean() / num_steps}", flush=True)
+
+        if i_episode % save_freq == 0:
+            ppo.save(os.path.join('runs/' + name + '/test_' + args.game, f"{i_episode}.pth"))
+            with open(os.path.join('runs/' + name + '/test_' + args.game, f"out_{i_episode}.json"), "w") as f:
+                json.dump(rew_means, f)
+            print(f"SAVING! {i_episode}")
+
+    ppo.save(os.path.join('runs/' + name + '/test_' + args.game, f"{i_episode}.pth"))
+    with open(os.path.join('runs/' + name + '/test_' + args.game, f"out_{i_episode}.json"), "w") as f:
+        json.dump(rew_means, f)
+    print(f"SAVING! {i_episode}")
